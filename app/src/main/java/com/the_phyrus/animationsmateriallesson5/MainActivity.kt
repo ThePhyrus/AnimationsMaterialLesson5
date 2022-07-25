f00c1532ffc3430c10e142b233a8547e297e9a23
ThePhyrus <romaisgood@yandex.ru>
2022-07-25 13:05:52 +0300

    Урок 5. Анимирование выполнено. Познакомились с TransitionManager

--- app/src/main/java/com/the_phyrus/animationsmateriallesson5/MainActivity.kt
+++ app/src/main/java/com/the_phyrus/animationsmateriallesson5/MainActivity.kt
@@ -3,6 +3,10 @@ package com.the_phyrus.animationsmateriallesson5
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.ChangeBounds
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.the_phyrus.animationsmateriallesson5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +20,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnAnimate.setOnClickListener {
+            val transitionFade = Fade()
+            transitionFade.duration = 3000
+
+            val transitionChangeBounds = ChangeBounds()
+            transitionChangeBounds.duration = 3000
+
+            val transitionSet = TransitionSet()
+            transitionSet.addTransition(transitionFade)
+            transitionSet.addTransition(transitionChangeBounds)
+
+//            TransitionManager.beginDelayedTransition(binding.transitionsContainer, transitionFade)
+//            TransitionManager.beginDelayedTransition(binding.transitionsContainer, transitionChangeBounds)
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer, transitionSet)
             binding.tvText.visibility = if (isGone) {
                 View.VISIBLE
             } else {
                 View.GONE
             }
             isGone = !isGone
+
         }
 
     }
