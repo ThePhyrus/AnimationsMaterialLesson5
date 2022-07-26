b9d87ab2069d57090d8ca12f0b96ea2b848a8461
ThePhyrus <romaisgood@yandex.ru>
2022-07-26 18:53:04 +0300

    Урок 5. constraintSet (два макета)рефактор закончен. Далее попробуем более правильный (по мнению Андрея) способ аниморовать изменения констрейнтов без создания второго файла разметки (меняем констрейнты в текущей разметке из кода)

--- app/src/main/java/com/the_phyrus/animationsmateriallesson5/BonusActivityDoubleLayout.kt
+++ app/src/main/java/com/the_phyrus/animationsmateriallesson5/BonusActivityDoubleLayout.kt
@@ -6,17 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.transition.ChangeBounds
 import androidx.transition.TransitionManager
-import com.the_phyrus.animationsmateriallesson5.databinding.ActivityBonusStartBinding
+import com.the_phyrus.animationsmateriallesson5.databinding.ActivityBonusDoubleLayoutStartBinding
 
-class BonusActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityBonusStartBinding
+class BonusActivityDoubleLayout : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBonusDoubleLayoutStartBinding
     private var isPressed: Boolean = false
     private val duration: Long = 1000L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityBonusStartBinding.inflate(layoutInflater)
+        binding = ActivityBonusDoubleLayoutStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.tap.setOnClickListener {
             isPressed = !isPressed
@@ -27,11 +28,11 @@ class BonusActivity : AppCompatActivity() {
             TransitionManager.beginDelayedTransition(binding.constraintContainer, transition)
             if (isPressed) {
                 binding.tap.text = "Hide description"
-                constraintSet.clone(this, R.layout.activity_bonus_end)
+                constraintSet.clone(this, R.layout.activity_bonus_double_layout_end)
                 constraintSet.applyTo(binding.constraintContainer)
             } else {
                 binding.tap.text = "Show description"
-                constraintSet.clone(this, R.layout.activity_bonus_start)
+                constraintSet.clone(this, R.layout.activity_bonus_double_layout_start)
                 constraintSet.applyTo(binding.constraintContainer)
             }
         }
